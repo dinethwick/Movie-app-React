88dde0ecf05993053eb0d9d99a1c0880791085c3
dinethwick <dinethwick23@gmail.com>
2025-07-20 18:39:00 +0930

    added and tested a search component.

--- movieapp/src/App.jsx
+++ movieapp/src/App.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/search'
 
 const App = () => {
+  // create a useState so we can pass the search prompt into the component
+  const [searchTerm, setSearchTerm] = useState('');
+
+
   return (
     <main>
       <div classname="pattern" />
@@ -10,6 +15,10 @@ const App = () => {
           <img src="hero.png" alt="Hero Banner" />
           <h1>Find <span className="text-gradient">Movies</span> You'll HATE without the Hassle</h1>
         </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h3 className="text-white">{searchTerm}</h3>
+
       </div>
     </main>
   )
---
+++ movieapp/src/components/Search.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+
+
+const Search = ({searchTerm, setSearchTerm}) => {
+    // do not change the props (arguments) that were passed into the component
+    // in this case, dont change the searchTerm (searchTerm = "I AM NOT BATMAN") etc
+    // changing it breaks the behaviour of react
+    return (
+        <div className="search">
+            <div>
+                <img src="search.svg" alt="search" />
+
+                <input 
+                    type="text"
+                    placeholder="Search through thousands of crappy movies"
+                    value={searchTerm}
+                    onChange={(event) => setSearchTerm(event.target.value)}
+                />
+
+            </div>
+        </div>
+    )
+}
+
+export default Search
+
+
+/*
+ * js - destructuring objects in js. this is used in react
+normal object:
+
+const person = {
+    name: 'Bruce Wayne',
+    age: 36,
+    location: 'Gotham'
+}
+
+normal access:
+    console.log(person.name)
+
+***destructuring:
+const { name, age, location } = person;
+
+then you can access normally without having to write person. everytime.
+console.log(name)
+
+*/
