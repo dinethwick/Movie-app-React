75ff0832b369dd5bfc80947f7ac93d0f825694cd
dinethwick <dinethwick23@gmail.com>
2025-07-20 19:48:30 +0930

    Adding API options to intergrate TheMovieDatabase's movie API

--- movieapp/src/App.jsx
+++ movieapp/src/App.jsx
@@ -1,6 +1,18 @@
 import React, { useState } from 'react'
 import Search from './components/search'
 
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
+
+
+
 const App = () => {
   // create a useState so we can pass the search prompt into the component
   const [searchTerm, setSearchTerm] = useState('');
