22824185ea7d09861777e4cfa6a42a323bda5b1c
dinethwick <dinethwick23@gmail.com>
2025-07-21 00:03:26 +0930

    added handling for cases where API returns Response: "False" instead of a HTTP code.
    I console logged the moveiList. It can now successfully fetch data.
    
    also declared setIsLoading to use later.

--- movieapp/src/App.jsx
+++ movieapp/src/App.jsx
@@ -18,24 +18,50 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   // for error handling
   const [errorMessage, setErrorMessage] = useState('');
+  // for movie list
+  const [movieList, setMovieList] = useState([]);
+  // for a is loading message
+  const [isLoading, setIsLoading] = useState(false)
+      
   
   const fetchMovies = async () => {
+    // initialise 'isLoading' and error message before running
+    setIsLoading(true);
+    setErrorMessage('');
+
     try{
+      // declare endpoint url and fetch data from it
       const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS)
       
+      // if response is not a HTTP OK, then throw error
       if(!response.ok) {
         throw new Error('failed to fetch')
       }
       
+      // response.json() -> reads the response body and parses it as JSON (turns it into a js object)
+      // await pauses execution until JSON is fully read and parsed
       const data = await response.json();
-      console.log(data);
+      // console.log(data);
 
+      // some API's return 'Response: "False"' instead of using HTTP status codes
+      // above case checks for HTTP code, this one checks for API returning 'Response: "False"'
+      // show error message + clear movie list + stop further execution by using return
+      if(data.Response === 'False') {
+        setErrorMessage(data.Error || 'Failed to fetch movies');
+        setMovieList([]);
+        return;
+      }
 
+      // if no error in fetching data, populate movie list
+      setMovieList(data.results || [])
 
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage('Error fetching movies. Please try again later.')
+    } finally {
+      // at this point we have already handled success and failure. so we stop the loading 
+      setIsLoading(false);
     }
   }
 
