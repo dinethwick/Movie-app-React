dfe4aed0347cab04c1c3baaeb9f23301cd7fa117
dinethwick <dinethwick23@gmail.com>
2025-07-23 18:26:12 +0930

    need to optimise search:     the default search functionality sends a query to the API     everytime a character is typed.     so if you want to search for 'Batman'     and you start typing b, then the app sends a query for 'b'     so for 1 search here, it does 6 queries.     this is not efficient. it could overwhelm the server.     if API tokens are priced, it could cost us alot of money
        we need to add a delay so that the user can type
        more of the movie name before the query is sent.
    
        this is called debouncing.
        it delays the request until the user has stopped typing
        for a predefined amount of time.

--- movieapp/src/components/Search.jsx
+++ movieapp/src/components/Search.jsx
@@ -26,6 +26,25 @@ export default Search
 
 
 /*
+    Search optimisation:
+    the default search functionality sends a query to the API 
+    everytime a character is typed.
+    so if you want to search for 'Batman'
+    and you start typing b, then the app sends a query for 'b'
+    so for 1 search here, it does 6 queries.
+    this is not efficient. it could overwhelm the server.
+    if API tokens are priced, it could cost us alot of money
+
+    we need to add a delay so that the user can type 
+    more of the movie name before the query is sent.
+
+    this is called debouncing.
+    it delays the request until the user has stopped typing
+    for a predefined amount of time.
+
+*/
+
+/*
  * js - destructuring objects in js. this is used in react
 normal object:
 
